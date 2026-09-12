c14a60fcd84a4297e7f224d1cc1e738a1e3e73b5
tomasagata <tomasagata00@gmail.com>
2026-09-12 17:39:00 +0000

    feat: add configuration for MongoDB and environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { OrdersModule } from './orders/orders.module.js';
 import { GeocodingModule } from './geocoding/geocoding.module.js';
 import { PaymentModule } from './payment/payment.module.js';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [OrdersModule, GeocodingModule, PaymentModule],
+  imports: [
+    OrdersModule, 
+    GeocodingModule, 
+    PaymentModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
+  ],
   providers: [],
   controllers: [],
 })
